dfd6cb6010ad58a9f40a436d4cced34ceb26f0d6
komorebivv <patrycja.stanislawczyk@gmail.com>
2021-08-27 17:53:41 +0200

    add djangorestframework to requirements.txt

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from plantsapp import views
+
+
+
+router = routers.DefaultRouter()
+router.register('categories', views.CategoryViewSet)
+router.register('rooms', views.RoomViewSet)
+router.register('plants', views.PlantViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
+    ]
